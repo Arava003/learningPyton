0b052c6a56ecf589630795832e2001d21b70fafd
arava <arava003@yandex.ru>
2021-01-24 18:01:17 +0500

    усовершенствовал калькулятор

--- HW24Jan.py
+++ HW24Jan.py
@@ -5,10 +5,10 @@ history_of_answers = []
 save_number = int()
 print("Здравствуйте. Вы зашли в приложение калькулятор. Вводите примеры в строку, если хотите узнать ответ")
 while True:
-    question = input("Введите 1, если хотите начать действия с новым числом\nВведите 2, если хотите продолжить работать со старым числом\n >>> ")
-    if question == "1":
-        examle = (input("Вводите пример >>> ").split())
-        print(examle)
+    examle = (input("Вводите пример >>> ").split())
+    print(examle)
+    print(len(examle))
+    if len(examle) == int(3):
         if examle[1] == "+":
             print(int(examle[0]) + int(examle[2]))
             history_of_answers.append(int(examle[0]) + int(examle[2]))
@@ -41,27 +41,7 @@ while True:
             print(int(examle[0]) ** int(examle[2]))
             history_of_answers.append(int(examle[0]) ** int(examle[2]))
             save_number = int(examle[0]) ** int(examle[2])
-        elif examle[0] == "sqrt" or examle[0] == "квадрат":
-            print(sqrt(int(examle[1])))
-            history_of_answers.append(sqrt(int(examle[1])))
-            save_number = sqrt(int(examle[1]))
-        elif examle[0] == "квадратный" and examle[1] == "корень":
-            print(sqrt(int(examle[2])))
-            history_of_answers.append(sqrt(int(examle[2])))
-            save_number = sqrt(int(examle[2]))
-        elif examle[0] == "sin" or examle[0] == "синус":
-            print(sin(int(examle[1])))
-            history_of_answers.append(sin(int(examle[1])))
-            save_number = sin(int(examle[1]))
-        elif examle[0] == "cos" or examle[0] == "косинус":
-            print(cos(int(examle[1])))
-            history_of_answers.append(cos(int(examle[1])))
-            save_number = cos(int(examle[1]))
-        else:
-            print("ERROR\nНевозможно выполнить действие")
-    elif question == "2":
-        examle = (input("Вводите пример >>> ").split())
-        print(examle)
+    elif len(examle) == int(2):
         if examle[0] == "+":
             print(save_number + int(examle[1]))
             history_of_answers.append(save_number + int(examle[1]))
@@ -95,13 +75,9 @@ while True:
             history_of_answers.append(save_number ** int(examle[1]))
             save_number = save_number ** int(examle[1])
         elif examle[0] == "sqrt" or examle[0] == "квадрат":
-            print(sqrt(save_number))
-            history_of_answers.append(sqrt(save_number))
-            save_number = sqrt(save_number)
-        elif examle[0] == "квадратный" and examle[1] == "корень":
-            print(sqrt(int(examle[2])))
-            history_of_answers.append(sqrt(int(examle[2])))
-            save_number = sqrt(int(examle[2]))
+            print(sqrt(int(examle[1])))
+            history_of_answers.append(sqrt(int(examle[1])))
+            save_number = sqrt(int(examle[1]))
         elif examle[0] == "sin" or examle[0] == "синус":
             print(sin(int(examle[1])))
             history_of_answers.append(sin(int(examle[1])))
@@ -112,7 +88,22 @@ while True:
             save_number = cos(int(examle[1]))
         else:
             print("ERROR\nНевозможно выполнить действие")
-    elif question == "print history" or question == "history" or question == "h" or question == "ph":
-        print(history_of_answers)
+    elif len(examle) == int(1):
+        if examle[0] == "sqrt" or examle[0] == "квадрат":
+            print(sqrt(save_number))
+            history_of_answers.append(sqrt(save_number))
+            save_number = sqrt(save_number)
+        elif examle[0] == "sin" or examle[0] == "синус":
+            print(sin(save_number))
+            history_of_answers.append(sin(save_number))
+            save_number = sin(save_number)
+        elif examle[0] == "cos" or examle[0] == "косинус":
+            print(cos(save_number))
+            history_of_answers.append(cos(save_number))
+            save_number = cos(save_number)
+        else:
+            print("ERROR\nНевозможно выполнить действие")
+    elif examle == "print history" or examle == "history" or examle == "h" or examle == "ph":
+        print("История ответов\n", history_of_answers)
     else:
         print("ERROR\nНеизвестная команда")
